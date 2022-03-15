954167a01c1de0b83fc6836ec36ffa8b9f0bdaa2
Spencer Tse <spencertse122@gmail.com>
2022-03-14 23:23:40 -0400

    p.140

--- meadowfield.js
+++ meadowfield.js
@@ -145,5 +145,32 @@ function routeRobot(state, memory) {
     if (memory.length == 0) {
         memory = mailRoute;
     }
+    // memory is an array, we will take the first one as direction
+    // and then use the slice function to trim off the position zero
     return {direction: memory[0], memory: memory.slice(1)};
+}
+
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
 }
