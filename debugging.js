dad7268957442d026c657ececfcc353c8b870d59
Spencer Tse <spencertse122@gmail.com>
2022-03-19 10:25:37 -0400

    p.147

--- debugging.js
+++ debugging.js
@@ -17,4 +17,35 @@ function numberToString(n, base = 10) {
 
 console.log(numberToString(13, 10));
 
+
 // Error Propagation
+// function Number(someInput) {
+//     return parseInt(someInput)
+// }
+
+// // these are pseudo code
+// // in node, it doesn't have Number() and prompt()
+// function promptNumber(question) {
+//     let result = Number(prompt(question));
+//     if (Number.isNaN(result)) return null;
+//     else return result;
+// }
+
+// console.log(promptNumber("How many trees do you see?"))
+
+// // other scenario
+
+function lastElement(array) {
+    if (array.length == 0){
+        return {failed: true};
+    } else {
+        return {element: array[array.length - 1]};
+    }
+}
+
+// setting a new array
+let testArray = [2,5,6,2,3,7]
+// see if it works
+console.log(lastElement(testArray))
+// see if it doesn't work, giving an empty array
+console.log(lastElement([]))
